34374eece123734b960e45eb3c70b4138fe51e80
david <david@DESKTOP-N7MELF5.Speedport_W_724V_01011603_07_002>
2021-05-05 19:17:11 +0200

    Änderung in Zeile 6

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 public void Main(String[] args) {
 	System.out.println("Hallo David");
 	System.out.println("Hallo XDavidX");
-	System.out.println("HeyHey");
+	System.out.println("HeyHey Dave");
+	
 }
 }
